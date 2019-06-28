5b47b163e45b3a584b477bf96da73ab9a1eea774
eduesqui <eduesqui87@gmail.com>
2019-06-28 16:54:12 -0500

    [ADD] Carga Inicial

---
+++ src/main/java/com/eduesqui/SpringBootHalActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.eduesqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHalActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHalActuatorApplication.class, args);
+	}
+
+}
